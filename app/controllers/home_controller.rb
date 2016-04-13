8ac686790cee53f9070a360930a31eb457f1f4e3
Eric Khoury <erickhoury@Erics-MacBook-Pro.local>
2016-04-13 15:25:39 +0200

    Adding dashboard wip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
 
   def index
+    if seller_signed_in? || buyer_signed_in?
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
   end
 end
