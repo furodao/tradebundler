048045dd79aa76e39dd3265bede953970c944754
Eric Khoury <erickhoury@Erics-MacBook-Pro.local>
2016-04-18 13:36:15 +0200

    Add status to offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,10 @@ class Offer < ApplicationRecord
   belongs_to :seller
   belongs_to :spec
   has_one :org, through: :seller
+
+  before_create :set_status_pending
+
+  def set_status_pending
+    self.status = :pending
+  end
 end
