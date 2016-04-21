6f90a2e94d6efc78434b58a4812db6a5443ea738
Eric Khoury <khoury@live.se>
2016-04-21 10:15:42 +0200

    Set row colors based on spec/offer status

--- app/models/spec.rb
+++ app/models/spec.rb
@@ -2,4 +2,8 @@ class Spec < ApplicationRecord
   belongs_to :buyer
   has_one :org, through: :buyer
   has_many :offers
+
+  def accepted?
+    self.offers.any? { |offer| offer.status == 'accepted' }
+  end
 end
