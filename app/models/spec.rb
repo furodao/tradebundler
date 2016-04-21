defdb74884fe60ec9dac1a96a476c8560005384e
Eric Khoury <khoury@live.se>
2016-04-21 15:39:48 +0200

    Tidy up rows for accepted specs as buyer

--- app/models/spec.rb
+++ app/models/spec.rb
@@ -4,6 +4,10 @@ class Spec < ApplicationRecord
   has_many :offers
   belongs_to :spec_category
 
+  def status
+    accepted? ? 'accepted' : nil
+  end
+
   def accepted?
     self.offers.any? { |offer| offer.status == 'accepted' }
   end
