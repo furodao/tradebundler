405c0e440c8c9c6dd7bfc7961fdd50b3df0a9f40
Eric Khoury <khoury@live.se>
2016-05-03 08:43:25 +0200

    Capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :ssh_options, {forward_agent: true}
 set :pty, true
 set :keep_releases, 5
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml }
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :default_run_options, { pty: true }
