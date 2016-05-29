fc519896e2b3a594247c3c0a73bd4058d7e6f10d
Eric Khoury <khoury@live.se>
2016-05-29 23:33:23 +0200

    Asset fixes

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Salesapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
   end
 end
