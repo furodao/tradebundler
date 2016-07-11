6b8a3663e3fdebd197a5f2c2b278e5c1b858c1fa
Eric Khoury <khoury@live.se>
2016-07-11 15:19:48 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,8 +43,8 @@ namespace :deploy do
     end
   end
 
-	after "bundle:install" do
-		run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
-	end
+	#after "bundle:install" do
+	#	run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+	#end
 
 end
