a2560916fcf84c91774180b07d60c2c104787dd8
Eric Khoury <khoury@live.se>
2016-07-14 22:21:08 +0200

    unicorn try fix

---
+++ unicorn.rb
@@ -0,0 +1,24 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/home/rails/salesapp/current/public"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/homes/rails/salesapp/current/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/home/rails/salesapp/current/log/unicorn.log"
+stdout_path "/homes/rails/salesapp/current/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.salesapp.sock"
+listen "/tmp/unicorn.salesapp.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
