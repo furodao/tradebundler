cd120d0fb081317384d912c7e8fc500225865218
Eric Khoury <khoury@live.se>
2016-07-17 22:01:59 +0200

    fix try

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,8 +47,11 @@ namespace :deploy do
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
+	after :restart do
     run "if [ -f /home/rails/salesapp/shared/tmp/pids/unicorn.pid ] && [ -e /proc/$(cat /home/rails/salesapp/shared/tmp/pids/unicorn.pid) ]; then kill -USR2 `cat /home/rails/salesapp/shared/tmp/pids/unicorn.pid`; else cd /home/rails/salesapp/current && bundle exec unicorn -c /home/rails/salesapp/current/unicorn.rb -E production -D; fi"
+  end
+
+  after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       #run "cd #{release_path}; RAILS_ENV=production bundle exec unicorn -c unicorn.rb -E production -D"
       # Here we can do anything such as:
